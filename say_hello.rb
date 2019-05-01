f0807fb7ffaf9a0a485133a53351491bb48247c3
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-01 21:04:15 +0000

    Default 1

--- say_hello.rb
+++ say_hello.rb
@@ -1,12 +1,16 @@
 # Build your say_hello method here
 
 
-def say_hello(name = "Hello Ruby Programmer!")
+def say_hello(name)
  
   puts "Hello #{name}!"
+  
+  def say_hello(name="Hello Ruby Programmer!")
+  
+  puts name
+end
 
 end
-say_hello
 
 
 
